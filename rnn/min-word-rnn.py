74e79ccb9aea408aff5b1f7aa8dd0a576791782c
liuzhi136 <liuzhi7936@sina.cn>
2016-07-08 10:54:09 +0800

    add a simple rnn example for Question-Anwser system, the train data is input1.txt

---
+++ rnn/min-word-rnn.py
@@ -0,0 +1,20 @@
+How are you doing? Hello.
+What are you doing? I am talking with you,
+Where are you from? I come from interjoy technology company.
+What's the weather today? Well, I really do not know.
+I am sorry that I broke your radio. Never mind.
+I didn't mean to be bump into you. That's all right.
+Do you know where the police station is? I don't know.
+The boss was every angry at the meeting. Why? Is there anything wrong?
+Could I borrow your car tomorrow? Sure! No problem.
+Did you know that Linda's uncle is Jacky Chen? You are joking! How did you know that?
+You can't go out today. Why not?
+Betty broke up with her new boyfriend. I don't understand how could this happen?
+You should take this form to the first counter. And then? What should I do?
+Do you know there is a new theater down the street? Yeah. So what?
+How's your steak? It's alright.
+Are you hurt? I am OK.
+We are leaving for Boston tonight. Bon voyage.
+Lucy is crying. Why? What happened?
+I don't want to go to the party. Come on! Don't be shy.
+#
